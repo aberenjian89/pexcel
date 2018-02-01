5930e113e6670117eb1031fd1ebda9caeedf7d01
Ali Berenjian <Ali.rberenjian@gmail.com>
2018-02-01 13:22:54 -0800

    Fixed sign up buttons

--- frontend/components/HomeFeed/header.jsx
+++ frontend/components/HomeFeed/header.jsx
@@ -13,7 +13,6 @@ class HomeFeed  extends React.Component{
                 <div className="header">
                     <div className="header-container">
                         <h3>Get inspired and share your best photos</h3>
-
                         <span>Find your home among the world's best photographers.</span>
                         <div className="header-container-link">
                             <Link activeClassName="signup" to="/signup">JOIN PEXCEL</Link>
