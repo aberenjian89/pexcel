583b7b1e3f44859118e6a1fa2840f515ea07bc58
Ali Berenjian <Ali.rberenjian@gmail.com>
2018-04-22 20:58:32 -0700

    Fixed Upload

--- frontend/components/profile/upload.jsx
+++ frontend/components/profile/upload.jsx
@@ -36,7 +36,7 @@ class Upload extends React.Component{
                 this.className = 'dropzone';
                 return false;
             }
-        })
+        }())
 
    
     }
@@ -91,7 +91,6 @@ class Upload extends React.Component{
             this.setState({images: array})
         };
 
-        debugger;
         if (file){
             fileReader.readAsDataURL(file)
         }
@@ -110,11 +109,76 @@ class Upload extends React.Component{
     }
 
     render(){
+       let images;
+       if (this.state.images.length > 0 ){
+            images = this.state.images.map((img,idx) =>{
+                return (
+                        <div className="img-container">
+                            <div>
+                                 <img src={img.url} key={idx} />
+                            </div>
+                            <div className="img-form">
+                                <div className="img-title-container">
+                                    <label> Title </label>
+                                    <input type="text" name="img_title" />
+                                </div>
+                                <div className="img-location-container">
+                                     <label> Location </label>
+                                     <input type="text" name="img_location" />
+                                </div>
+                                <div className="img-desc-container">
+                                     <label> Description </label>
+                                     <input type="text" name="img_desc" />
+                                </div>
+                                <div className="img-datataken-container"> 
+                                     <label> Date Taken </label>
+                                     <input type="text" name="date_taken" />
+                                </div>
+                                <div>
+                                    <label> Category </label>
+                                    <select name="category">
+                                        <option value="uncategorized">Uncategorized</option>
+                                        <option value="abstract">Abstract</option>
+                                        <option value="aerial">Aerial</option>
+                                        <option value="animals">Animals</option>
+                                        <option value="black&white">Black & White</option>
+                                        <option value="celebrities">Celebrities</option>
+                                        <option value="city&architecture">City & Architecture</option>
+                                        <option value="commercial">Commerical</option>
+                                        <option value="concert">Concert</option>
+                                        <option value="family">Family</option>
+                                        <option value="fashion">Fashion</option>
+                                        <option value="film">Film</option>
+                                        <option value="fineart">Fine Art</option>
+                                        <option value="food">Food</option>
+                                        <option value="landscapes">Landscapes</option>
+                                        <option value="nature">Natures</option>
+                                        <option value="night">Night</option>
+                                        <option value="people">People</option>
+                                        <option value="performingarts">Performing Arts</option>
+                                        <option value="sport">sports</option>
+                                        <option value="stillife">Still Life</option>
+                                        <option value="street">Street</option>
+                                        <option value="transportation">Transportation</option>
+                                        <option value="travel">Travel</option>
+                                        <option value="underwater">Under Water</option>
+                                        <option value="wedding">Wedding</option>
+                                        <option value="nude">Nude</option>
+                                    </select>
+                                </div> 
+                            </div>                          
+                        </div>
+                    );
+            })
+       }
        return (
             <div className="upload-container">
                 <div className="dropzone" id="dropzone">
                     <label> Drag & Drops or Browse </label>
                     <input type="file"  name="myFile" multiple onChange={this.readfile}/>       
+                </div>
+                <div className="images-container">
+                    {images}
                 </div>
             </div>
         )
