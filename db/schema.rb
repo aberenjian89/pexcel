10a6ad8c2555d57e1a321c43198957516db5e10c
Ali Berenjian <ali.rberenjian@gmail.com>
2019-03-28 21:15:35 -0700

    Remove the All Columns from User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190329025147) do
+ActiveRecord::Schema.define(version: 2019_03_29_041150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,12 +60,6 @@ ActiveRecord::Schema.define(version: 20190329025147) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "avatar_file_name"
-    t.string "avatar_content_type"
-    t.integer "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
 end
