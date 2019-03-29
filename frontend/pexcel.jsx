d6f291aeb5d1b59d5ca602afb83fb3ba3f7c2596
Alireza Berenjian <Ali.rberenjian@gmail.com>
2019-03-28 19:47:55 -0700

    Add Store and Reducer and Compoenets

--- frontend/pexcel.jsx
+++ frontend/pexcel.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded",()=>{
-    // let preloadstate =undefined;
-    // if (window.currentUser){
-    //     preloadstate={
-    //         session:{
-    //             CurrentUser: window.currentUser
-    //         }
-    //     };
-    // }
-    // const store = configureStore(preloadstate);
+    let preloadstate =undefined;
+    if (window.currentUser){
+        preloadstate={
+            session:{
+                CurrentUser: window.currentUser
+            }
+        };
+    }
+    const store = configureStore(preloadstate);
     // window.store=store;
     // window.dispatch = store.dispatch;
     // window.getState = store.getState;
---
+++ frontend/store/store.js
@@ -0,0 +1,25 @@
+import {createStore,applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import RootReducer from '../reducer/root_reducer'
+import logger from 'redux-logger'
+
+const middleware = [thunk]
+
+
+if (process.env.NODE_ENV !== 'production') {
+    // must use 'require' (import only allowed at top of file)
+
+    middleware.push(logger);
+}
+
+
+const configureStore=(preloadedstate={})=>(
+    createStore(
+        RootReducer,
+        preloadedstate,
+        applyMiddleware(...middleware)
+    )
+);
+
+
+export default configureStore;
