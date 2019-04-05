cda14e49b574dd91df6f203f1fd932f017d9c378
Ali Berenjian <ali.rberenjian@gmail.com>
2019-04-05 14:51:27 -0700

    Add First Step

--- frontend/components/upload_component/upload_component.jsx
+++ frontend/components/upload_component/upload_component.jsx
@@ -1,253 +1,305 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import Slide from '@material-ui/core/Slide';
-import { withStyles } from '@material-ui/core/styles';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepButton from '@material-ui/core/StepButton';
-import Typography from '@material-ui/core/Typography';
-import StepLabel from '@material-ui/core/StepLabel'
-import GetApp from '@material-ui/icons/GetApp'
-import Input from '@material-ui/core/Input'
-
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import Slide from "@material-ui/core/Slide";
+import { withStyles } from "@material-ui/core/styles";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepButton from "@material-ui/core/StepButton";
+import Typography from "@material-ui/core/Typography";
+import StepLabel from "@material-ui/core/StepLabel";
+import GetApp from "@material-ui/icons/GetApp";
+import Input from "@material-ui/core/Input";
 
 function Transition(props) {
-    return <Slide direction="up" {...props} />;
+  return <Slide direction="up" {...props} />;
 }
 
 const styles = theme => ({
-    root: {
-        // width: '90%',
+  root: {
+    // width: '90%',
+  },
+  button: {
+    marginRight: theme.spacing.unit
+  },
+  completed: {
+    display: "inline-block"
+  },
+  instructions: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    marginLeft: theme.spacing.unit,
+    display: "flex",
+    justifyContent: "center"
+  },
+  form_content: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  upload_area: {
+    minWidth: "25%",
+    minHeight: "250px",
+    height: "70%",
+    width: "70%",
+    border: "2px dashed #9e9e9e",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  dialog: {
+    minHeight: "300px !important"
+  },
+  upload_content: {
+    "& input": {
+      "font-size": "100px",
+      position: "absolute",
+      left: 0,
+      top: 0,
+      opacity: 0
     },
-    button: {
-        marginRight: theme.spacing.unit,
-    },
-    completed: {
-        display: 'inline-block',
-    },
-    instructions: {
-        marginTop: theme.spacing.unit,
-        marginBottom: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-        marginLeft: theme.spacing.unit,
-        display: 'flex',
-        justifyContent: 'center'
-    },
-    upload_area:{
-        minWidth: '25%',
-        minHeight: '250px',
-        height: '70%',
-        width: '70%',
-        border: '2px dashed #9e9e9e',
-    },
-    dialog:{
-      minHeight: "300px !important",
-    },
-    upload_content:{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignContent: 'center'
+    position: "relative",
+    overflow: "hidden",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    height: "100px",
+    "& div": {
+      display: "flex",
+      justifyContent: "center"
     }
+  }
 });
 
 function getSteps() {
-    return ['Select photos', 'Edit Information', 'Review'];
+  return ["Select photos", "Edit Information", "Review"];
 }
 
-function getStepContent(step,props) {
-    const {classes} = props;
-    switch (step) {
-        case 0:
-            return (
-                <div className={classes.upload_area}>
-                    <form>
-                        <div className={classes.upload_content}>
-                            <div>
-                                <GetApp/>
-                            </div>
-                            <Input type="file" multiple>Select files</Input>
-                        </div>
-                    </form>
+function getStepContent(step, props) {
+  const { classes } = props;
+  switch (step) {
+    case 0:
+      return (
+        <div className={classes.upload_area} id="drop_area">
+          <form>
+            <div className={classes.upload_content}>
+              <div>
+                <GetApp fontSize="large" fontSizeAdjust="100px" />
+              </div>
+              <div>
+                <input type="file" multiple />
+              </div>
+              <div>
+                <label>Select a file or drag a it here</label>
+              </div>
+            </div>
+          </form>
+        </div>
+      );
+    case 1:
+      return "Step 2: Add photo(s) information";
+    case 2:
+      return "Step 3: Review your photo(s)";
+    default:
+      return "Unknown step";
+  }
+}
+
+class UploadComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      activeStep: 0,
+      completed: {}
+    };
+    this.handleClose = this.handleClose.bind(this);
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleNext = this.handleNext.bind(this);
+    this.handleBack = this.handleBack.bind(this);
+    this.handleStep = this.handleStep.bind(this);
+    this.handleComplete = this.handleComplete.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.completedSteps = this.completedSteps.bind(this);
+    this.isLastStep = this.isLastStep.bind(this);
+    this.allStepsCompleted = this.allStepsCompleted.bind(this);
+    this.handleFiles = this.handleFiles.bind(this);
+  }
+
+  componentDidMount(props) {
+    // let drop_area = document.getElementById("drop_area");
+    // debugger;
+    // drop_area.addEventListener(
+    //   "drop",
+    //   e => {
+    //     let dt = e.dataTransfer;
+    //     let files = dt.files;
+    //     debugger;
+    //     this.handleFiles(files);
+    //   },
+    //   false
+    // );
+  }
+
+  handleFiles(files) {
+    [...files].forEach(file => {
+      let formdata = new FormData();
+      formdata.append(file);
+    });
+  }
+
+  totalSteps() {
+    getSteps().length;
+  }
+
+  componentWillReceiveProps(nextprops) {
+    if (nextprops.ModalStatus != this.state.open) {
+      this.setState({
+        open: nextprops.ModalStatus
+      });
+    }
+  }
+
+  handleNext() {
+    let activeStep;
+
+    if (this.isLastStep() && !this.allStepsCompleted()) {
+      // It's the last step, but not all steps have been completed,
+      // find the first step that has been completed
+      const steps = getSteps();
+      activeStep = steps.findIndex((step, i) => !(i in this.state.completed));
+    } else {
+      activeStep = this.state.activeStep + 1;
+    }
+    this.setState({
+      activeStep
+    });
+  }
+
+  handleBack() {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1
+    }));
+  }
+
+  handleStep(step) {
+    return () => {
+      this.setState({
+        activeStep: step
+      });
+    };
+  }
+
+  handleComplete() {
+    const { completed } = this.state;
+    completed[this.state.activeStep] = true;
+    this.setState({
+      completed
+    });
+    this.handleNext();
+  }
+
+  handleReset() {
+    this.setState({
+      activeStep: 0,
+      completed: {}
+    });
+  }
+
+  completedSteps() {
+    return Object.keys(this.state.completed).length;
+  }
+
+  isLastStep() {
+    return this.state.activeStep === this.totalSteps() - 1;
+  }
+
+  allStepsCompleted() {
+    return this.completedSteps() === this.totalSteps();
+  }
+
+  handleClickOpen() {
+    this.setState({ open: true });
+  }
+
+  handleClose() {
+    this.setState({ open: false });
+    this.handleReset();
+    this.props.ModalHide();
+  }
+
+  render() {
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+
+    return (
+      <Dialog
+        open={this.state.open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={this.handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+        fullWidth={true}
+        maxWidth="md"
+        className={classes.dialog}
+      >
+        <DialogContent>
+          <div className={classes.root}>
+            <Stepper activeStep={activeStep} alternativeLabel>
+              {steps.map(label => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+            <div>
+              {this.state.activeStep === steps.length ? (
+                <div>
+                  <Typography className={classes.instructions}>
+                    All steps completed
+                  </Typography>
+                  <Button onClick={this.handleReset}>Reset</Button>
                 </div>
-            );
-        case 1:
-            return 'Step 2: Add photo(s) information';
-        case 2:
-            return 'Step 3: Review your photo(s)';
-        default:
-            return 'Unknown step';
-    }
-}
-
-
-class UploadComponent extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            open: false,
-            activeStep: 0,
-            completed: {}
-        };
-        this.handleClose = this.handleClose.bind(this);
-        this.handleClickOpen = this.handleClickOpen.bind(this);
-        this.handleNext = this.handleNext.bind(this);
-        this.handleBack = this.handleBack.bind(this);
-        this.handleStep = this.handleStep.bind(this);
-        this.handleComplete = this.handleComplete.bind(this);
-        this.handleReset = this.handleReset.bind(this);
-        this.completedSteps = this.completedSteps.bind(this);
-        this.isLastStep = this.isLastStep.bind(this);
-        this.allStepsCompleted = this.allStepsCompleted.bind(this);
-
-
-    }
-
-    totalSteps(){
-        getSteps().length;
-    }
-
-    componentWillReceiveProps(nextprops){
-        if (nextprops.ModalStatus != this.state.open){
-            this.setState({
-                open: nextprops.ModalStatus
-            })
-        }
-    }
-
-    handleNext(){
-        let activeStep;
-
-        if (this.isLastStep() && !this.allStepsCompleted()) {
-            // It's the last step, but not all steps have been completed,
-            // find the first step that has been completed
-            const steps = getSteps();
-            activeStep = steps.findIndex((step, i) => !(i in this.state.completed));
-        } else {
-            activeStep = this.state.activeStep + 1;
-        }
-        this.setState({
-            activeStep,
-        });
-    };
-
-    handleBack(){
-        this.setState(state => ({
-            activeStep: state.activeStep - 1,
-        }));
-    };
-
-    handleStep(step) {
-        return () => {
-            this.setState({
-                activeStep: step,
-            });
-        }
-    };
-
-    handleComplete(){
-        const { completed } = this.state;
-        completed[this.state.activeStep] = true;
-        this.setState({
-            completed,
-        });
-        this.handleNext();
-    };
-
-    handleReset(){
-        this.setState({
-            activeStep: 0,
-            completed: {},
-        });
-    };
-
-    completedSteps() {
-        return Object.keys(this.state.completed).length;
-    }
-
-    isLastStep() {
-        return this.state.activeStep === this.totalSteps() - 1;
-    }
-
-    allStepsCompleted() {
-        return this.completedSteps() === this.totalSteps();
-    }
-
-
-    handleClickOpen(){
-        this.setState({ open: true });
-    };
-
-    handleClose(){
-        this.setState({ open: false });
-        this.handleReset()
-        this.props.ModalHide()
-    };
-
-    render(){
-        const { classes } = this.props;
-        const steps = getSteps();
-        const { activeStep } = this.state;
-
-        return(
-            <Dialog
-                open={this.state.open}
-                TransitionComponent={Transition}
-                keepMounted
-                onClose={this.handleClose}
-                aria-labelledby="alert-dialog-slide-title"
-                aria-describedby="alert-dialog-slide-description"
-                fullWidth={true}
-                maxWidth="md"
-                className={classes.dialog}
-            >
-                <DialogContent>
-                    <div className={classes.root}>
-                        <Stepper activeStep={activeStep} alternativeLabel>
-                            {steps.map(label => (
-                                <Step key={label}>
-                                    <StepLabel>{label}</StepLabel>
-                                </Step>
-                            ))}
-                        </Stepper>
-                        <div>
-                            {this.state.activeStep === steps.length ? (
-                                <div>
-                                    <Typography className={classes.instructions}>All steps completed</Typography>
-                                    <Button onClick={this.handleReset}>Reset</Button>
-                                </div>
-                            ) : (
-                                <div>
-                                    <div className={classes.instructions}>{getStepContent(activeStep,this.props)}</div>
-                                    <div>
-                                        <Button
-                                            disabled={activeStep === 0}
-                                            onClick={this.handleBack}
-                                            className={classes.backButton}
-                                        >
-                                            Back
-                                        </Button>
-                                        <Button variant="contained" color="primary" onClick={this.handleNext}>
-                                            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                                        </Button>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </DialogContent>
-            </Dialog>
-        )
-    }
+              ) : (
+                <div>
+                  <div className={classes.instructions}>
+                    {getStepContent(activeStep, this.props)}
+                  </div>
+                  <div>
+                    <Button
+                      disabled={activeStep === 0}
+                      onClick={this.handleBack}
+                      className={classes.backButton}
+                    >
+                      Back
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.handleNext}
+                    >
+                      {activeStep === steps.length - 1 ? "Finish" : "Next"}
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+    );
+  }
 }
 
 UploadComponent.propTypes = {
-    classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
-
 
 export default withStyles(styles)(UploadComponent);
