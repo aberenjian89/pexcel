08089078a47729d30ccdbc6b961160fd7dc346d2
Ali Berenjian <ali.rberenjian@gmail.com>
2019-05-11 23:20:58 -0700

    Remove the column migration

--- db/migrate/20190409203624_change_image_table.rb
+++ db/migrate/20190409203624_change_image_table.rb
@@ -5,10 +5,6 @@ class ChangeImageTable < ActiveRecord::Migration[5.2]
     remove_column :images, :author_id
     remove_column :images, :date_taken
     remove_column :images, :category
-    remove_column :images, :img_file_name
-    remove_column :images, :img_content_type
-    remove_column :images, :img_file_size 
-    remove_column :images, :img_updated_at
     remove_column :images, :width
     remove_column :images, :height
     add_column :images, :name, :string, null: false
