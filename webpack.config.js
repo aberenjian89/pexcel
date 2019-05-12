ccdb6668e3af49cb72e397f979541f965240cc5a
Ali Berenjian <ali.rberenjian@gmail.com>
2019-05-11 22:46:38 -0700

    Remove minizaer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const webpack = require("webpack");
-const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
 
 plugins = []; // if using any plugins for both dev and production
 let devPlugins = []; // if using any plugins for development
@@ -39,44 +38,6 @@ module.exports = {
           }
         }
       }
-    ]
-  },
-  optimization: {
-    minimizer: [
-      new HardSourceWebpackPlugin({
-        // Either an absolute path or relative to webpack's options.context.
-        cacheDirectory: "node_modules/.cache/hard-source/[confighash]",
-        // Either a string of object hash function given a webpack config.
-        configHash: function(webpackConfig) {
-          // node-object-hash on npm can be used to build this.
-          return require("node-object-hash")({ sort: false }).hash(
-            webpackConfig
-          );
-        },
-        // Either false, a string, an object, or a project hashing function.
-        environmentHash: {
-          root: process.cwd(),
-          directories: [],
-          files: ["package-lock.json", "yarn.lock"]
-        },
-        // An object.
-        info: {
-          // 'none' or 'test'.
-          mode: "none",
-          // 'debug', 'log', 'info', 'warn', or 'error'.
-          level: "debug"
-        },
-        // Clean up large, old caches automatically.
-        cachePrune: {
-          // Caches younger than `maxAge` are not considered for deletion. They must
-          // be at least this (default: 2 days) old in milliseconds.
-          maxAge: 2 * 24 * 60 * 60 * 1000,
-          // All caches together must be larger than `sizeThreshold` before any
-          // caches will be deleted. Together they must be at least this
-          // (default: 50 MB) big in bytes.
-          sizeThreshold: 50 * 1024 * 1024
-        }
-      })
     ]
   },
   devtool: "source-map",
